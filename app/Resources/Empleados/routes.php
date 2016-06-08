d2f18e03f679bc1c608d47a5c325e4c8ba982d92
Gustavo Pérez Machado <gustperz@gmail.com>
2016-06-08 09:37:59 -0500

    rutas empleados

--- app/Resources/Empleados/routes.php
+++ app/Resources/Empleados/routes.php
@@ -8,11 +8,13 @@
 $api->group(
     ['prefix' => 'empleados', 'namespace' => 'App\Resources\Empleados\Controllers'],
     function () use ($api) {
-        $api->get('/', 'EmpleadosController@index');
-        $api->get('/syncup', 'SyncController@up');
-        $api->get('/{cedula}', 'EmpleadosController@show');
-        $api->get('/{cedula}/certificado_laboral/{tipo}', 'CertificadosController@create');
+        $api->get('/syncup', ['middleware' => 'auth_su', 'uses' => 'SyncController@up']);
+        $api->group(['middleware' => 'api.auth'], function () use ($api){
+            $api->get('/', 'EmpleadosController@index');
+            $api->get('/{cedula}', 'EmpleadosController@show');
+            $api->get('/{cedula}/certificado_laboral/{tipo}', 'CertificadosController@create');
+        });
     }
 );
 
-$api->get('/probando_probando', 'App\Resources\Empleados\Controllers\EmpleadosController@test');
+//$api->get('/probando_probando', 'App\Resources\Empleados\Controllers\EmpleadosController@test');
